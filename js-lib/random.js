5820d99a585a4379762684badb6dd9d2fc118566
ybZhang <472099249@qq.com>
2020-11-12 23:13:20 +0800

    continue

---
+++ js-lib/random.js
@@ -0,0 +1,42 @@
+function randomRange(myMax, myMin) {
+    return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin
+}
+
+function randomInteger(num) {
+    return Math.floor(Math.random() * num)
+}
+
+// 字符串拼接的空格 "\xa0"
+
+//map() reduce() filter()
+
+pop()
+shift()
+push()
+unshift()
+slice()
+splice()
+indexOf()
+map()
+// 以任意顺序遍历一个对象的除Symbol以外的可枚举属性。
+for (const key in object) {
+    if (object.hasOwnProperty(key)) {
+        const element = object[key];
+        
+    }
+};
+for (const iterator of object) {
+    
+};
+array.forEach(element => {
+    
+});
+
+let arr = [...rest]
+//delete 用来删除对象的属性
+//一般都是用.语法来访问对象的属性，访问的属性为变量时，使用[]语法
+
+Object.keys()
+
+hasOwnProperty()
+prototype.isPrototypeOf()

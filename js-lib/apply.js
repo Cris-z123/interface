a7fe7ba1b11cb2142324adb97f978b3fb6939adc
rookie996-object <472099249@qq.com>
2021-01-24 23:55:27 +0800

    fix

--- js-lib/apply.js
+++ js-lib/apply.js
@@ -1,4 +1,8 @@
-// apply() 方法调用一个具有给定this值的函数，以及以一个数组（或类数组对象）的形式提供的参数。
+/**
+ * apply() 方法调用一个具有给定this值的函数，以及以一个数组（或类数组对象）的形式提供的参数。
+ * @param {*绑定值} context 
+ * @param {*参数} args 
+ */
 Function.prototype.myApply = function(context, args) {
     context = context || window;
     args = args ? args : [];
--- js-lib/call.js
+++ js-lib/call.js
@@ -1,4 +1,8 @@
-// call() 方法使用一个指定的 this 值和单独给出的一个或多个参数来调用一个函数。
+/**
+ * call() 方法使用一个指定的 this 值和单独给出的一个或多个参数来调用一个函数。
+ * @param {*绑定值} context 
+ * @param  {...any 参数} args 
+ */
 
 Function.prototype.myCall = function(context, ...args) {
     context = context || window;

3c89148ee8c6810a96bc0e057686da2036df4a77
rookie996-object <472099249@qq.com>
2021-11-29 23:02:55 +0800

    doc：Promise practice

--- doc/reading-practice.js
+++ doc/reading-practice.js
@@ -24,3 +24,53 @@ const TwoDimensionalArray = [
 TwoDimensionalArray.reduce((acc, cur) => {
   return acc.concat(cur);
 }, []);
+
+function typeValidator(x) {
+  if (x === null) return "null";
+
+  if (typeof x !== "object") return typeof x;
+
+  return Object.prototype.toString.call(x).slice(8, -1).toLowerCase();
+}
+
+function red() {
+  console.log("red");
+}
+function green() {
+  console.log("green");
+}
+function yellow() {
+  console.log("yellow");
+}
+
+const determineTrafficLight = (light) => {
+  if (light === "red") {
+    red();
+  } else if (light === "green") {
+    green();
+  } else if (light === "yellow") {
+    yellow();
+  }
+};
+
+const trafficLight = (timer, light) =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(determineTrafficLight(light));
+    }, timer);
+  });
+
+const step = () => {
+  trafficLight(3000, "red")
+    .then(() => trafficLight(1000, "green"))
+    .then(() => trafficLight(2000, "yellow"))
+    .then(step)
+    .catch((e) => new Error());
+};
+
+const step = async () => {
+  await trafficLight(3000, "red");
+  await trafficLight(1000, "green");
+  await trafficLight(2000, "yellow");
+  step();
+};

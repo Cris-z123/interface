61e17e10b4c2d1508d389519e7c09e283fc5ae95
rookie996-object <472099249@qq.com>
2021-12-05 22:48:45 +0800

    doc：js practice

--- doc/reading-practice.js
+++ doc/reading-practice.js
@@ -1,3 +1,8 @@
+/**
+ * 顺序执行多个函数
+ * @param  {Function} funcs 函数
+ * @returns
+ */
 function compose(...funcs) {
   if (funcs.length === 0) {
     return (arg) => arg;
@@ -16,6 +21,9 @@ function compose(...funcs) {
 
 compose(console.log(1), console.log(2));
 
+/**
+ * 二维数组拉平
+ */
 const TwoDimensionalArray = [
   [1, 1],
   [2, 2],
@@ -25,6 +33,11 @@ TwoDimensionalArray.reduce((acc, cur) => {
   return acc.concat(cur);
 }, []);
 
+/**
+ * 数据类型校验器
+ * @param {any} x
+ * @returns
+ */
 function typeValidator(x) {
   if (x === null) return "null";
 
@@ -73,4 +86,109 @@ const step = async () => {
   await trafficLight(1000, "green");
   await trafficLight(2000, "yellow");
   step();
+};
+
+/**
+ * 并发请求，并限制最大并发数
+ * @param {String || Array} urlIds 接口地址
+ * @param {Promise} loadImg 接口请求
+ * @param {Number} limit 最大并发限制数
+ * @returns
+ */
+const loadByLimit = (urlIds, loadImg, limit) => {
+  const urlIdsCopy = [...urlIds];
+
+  if (urlIdsCopy.length <= limit) {
+    const promiseArray = urlIds.map((urlId) => loadImg(urlId));
+    return Promise.all(promiseArray);
+  }
+
+  const promiseArray = urlIdsCopy
+    .splice(0, limit)
+    .map((urlId) => loadImg(urlId));
+
+  urlIdsCopy
+    .reduce(
+      (prevPromise, urlId) =>
+        prevPromise
+          .then(() => Promise.race(promiseArray))
+          .catch((error) => console.log(error))
+          .then((resolvedId) => {
+            let resolvedIdPosition = promiseArray.findIndex(
+              (id) => resolvedId === id
+            );
+
+            promiseArray.splice(resolvedIdPosition, 1);
+            promiseArray.push(loadImg(urlId));
+          }),
+      Promise.resolve()
+    )
+    .then(() => Promise.all(promiseArray));
+};
+
+/**
+ * my promise
+ * @param {*} executor  执行器
+ */
+function Promise(executor) {
+  this.status = "pending";
+  this.value = null;
+  this.reason = null;
+
+  this.onFulfilledArray = [];
+  this.onRejectedArray = [];
+
+  const resolve = (value) => {
+    if (value instanceof Promise) {
+      return value.then(resolve, reject);
+    }
+    setTimeout(() => {
+      if (this.status === "pending") {
+        this.value = value;
+        this.status = "fulfilled";
+
+        this.onFulfilledArray.forEach((func) => func(value));
+      }
+    });
+  };
+
+  const reject = (reason) => {
+    setTimeout(() => {
+      if (this.status === "pending") {
+        this.reason = reason;
+        this.status = "rejected";
+
+        this.onRejectedArray.forEach((func) => func(reason));
+      }
+    });
+  };
+
+  try {
+    executor(resolve, reject);
+  } catch (e) {
+    reject(e);
+  }
+}
+
+Promise.prototype.then = function (onfulfilled, onrejected) {
+  onfulfilled =
+    typeof onfulfilled === "function" ? onfulfilled : (data) => data;
+
+  onrejected =
+    typeof onrejected === "function"
+      ? onrejected
+      : (error) => {
+          throw error;
+        };
+
+  if (this.status === "fulfilled") {
+    onfulfilled(this.value);
+  }
+  if (this.status === "rejected") {
+    onrejected(this.reason);
+  }
+  if (this.status === "pending") {
+    this.onFulfilledArray.push(onfulfilled);
+    this.onRejectedArray.push(onrejected);
+  }
 };

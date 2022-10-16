aac475f04daf26dabd56d6728d85225459c0aaaf
rookie996-object <472099249@qq.com>
2022-10-16 23:00:01 +0800

    feat: MyNew function

--- js-lib/new.js
+++ js-lib/new.js
@@ -5,9 +5,26 @@
  * 3.将对象的this绑定返回值上
  * 4.如果函数没有返回值，就返回这个新对象，否则返回值
  */
-function myNew() {
-    let obj = new Object();
-    obj._proto_ = ctx.prototype;
-    let res = ctx.call(obj, ...args)
-    return res instanceof Object ? res : obj
+function myNew1() {
+  let obj = new Object();
+  obj._proto_ = ctx.prototype;
+  let res = ctx.call(obj, ...args);
+  return res instanceof Object ? res : obj;
+}
+
+function myNew2() {
+  let newObject = null;
+  let constructor = Array.prototype.shift.call(arguments);
+  let result = null;
+
+  if (typeof constructor !== "function") {
+    console.error("type error");
+    return;
+  }
+
+  newObject = Object.create(constructor.prototype);
+  result = constructor.apply(newObject, arguments);
+  let flag =
+    result && (typeof result === "object" || typeof result === "function");
+  return flag ? result : newObject;
 }

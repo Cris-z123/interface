b4e34c6d28275a85711043a898450ef6c0561577
rookie996-object <472099249@qq.com>
2022-10-30 23:01:01 +0800

    doc: linkList

---
+++ leetcode/linkList.js
@@ -0,0 +1,61 @@
+/**
+ * 链表类
+ * @param {*} val 
+ */
+function LinkList(val) {
+    this.val = val
+    this.next = null
+}
+
+/**
+ * 输入一个链表，输出该链表中倒数第k个节点。为了符合大多数人的习惯，本题从1开始计数，即链表的尾节点是倒数第1个节点。
+ * @param {*} head 
+ * @param {*} k 
+ */
+function getKthFormEnd(head, k) {
+
+}
+
+/**
+ * 输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。
+ * @param {*} head 
+ */
+function reversePrint(head) {
+    let result = []
+    while(head !== null) {
+        result.unshift(head.val)
+        head = head.next
+    }
+    return result
+}
+
+/**
+ * 给定单向链表的头指针和一个要删除的节点的值，定义一个函数删除该节点。
+ * 返回删除后的链表的头节点。
+ * @param {*} head 
+ * @param {*} val 
+ */
+function deleteNode(head, val) {
+
+}
+
+
+/**
+ * 输入一个链表的头节点，反转该链表并输出反转后链表的头节点
+ * @param {*} head 
+ * @returns 
+ */
+function reversList(head) {
+    let first = head
+    let last = null
+
+    while(first !== null) {
+        const next = first.next
+
+        first.next = last
+        last = first
+        first = next
+    }
+
+    return last
+}

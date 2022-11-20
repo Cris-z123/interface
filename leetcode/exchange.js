51db3bdb11ceb139000d782c833fde2ec8128ecd
rookie996-object <472099249@qq.com>
2022-11-20 23:03:33 +0800

    feat: leetcode-exchange

---
+++ leetcode/exchange.js
@@ -0,0 +1,21 @@
+/**
+ * 调整数组顺序使奇数位于偶数前面
+ * @param {array} nums 
+ * @returns 
+ */
+
+const exchange = nums => {
+    let [left, right] = [0, nums.length - 1]
+
+    while(left < right) {
+        while(left < right && nums[left] & 1) {
+            left++
+        }
+        while(left < right && nums[right] & 1) {
+            right--
+        }
+        [nums[left], nums[right]] = [[nums[right]], nums[left]]
+    }
+
+    return nums
+}

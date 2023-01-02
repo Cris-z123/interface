460f02fa3d1ec71f6885dc9995147925ff3480e4
rookie996-object <472099249@qq.com>
2023-01-02 22:50:18 +0800

    perf: perf object text

--- js-lib/Object.js
+++ js-lib/Object.js
@@ -16,7 +16,7 @@ class Thermostat {
     }
 }
 
-const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+const thermos = new Thermostat(76);
 let temp = thermos.temperature; // 24.44 in Celsius
 thermos.temperature = 26;
 temp = thermos.temperature; // 26 in Celsius

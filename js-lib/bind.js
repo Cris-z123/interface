c9d0ae1465facd6235b6f5d99153abffb88ac6d8
rookie996-object <472099249@qq.com>
2023-07-09 23:25:43 +0800

    feat: bind function add note

--- js-lib/bind.js
+++ js-lib/bind.js
@@ -1,7 +1,10 @@
 /**
+ * bind()
  * bind() 方法创建一个新的函数，在 bind() 被调用时，这个新函数的 this 被指定为 bind() 的第一个参数而其余参数将作为新函数的参数，供调用时使用。
+ * @param {*} context 
+ * @param {*} args 
+ * @returns 
  */
-
 Function.prototype.myBind = function (context, args) {
   const fn = this;
   args = args ? args : [];
@@ -18,7 +21,7 @@ Function.prototype.myBind =
   function (context) {
     let me = this;
     let args = Array.prototype.slice.call(arguments, 1);
-    let F = function () {};
+    let F = function () { };
 
     F.prototype = this.prototype;
     const bound = function () {

2b672172d223259782454c15942a83f1519bbf85
rookie996-object <472099249@qq.com>
2023-07-23 23:27:43 +0800

    feat: add note

--- js-lib/calculation.js
+++ js-lib/calculation.js
@@ -1,3 +1,9 @@
+/**
+ * 精准加法
+ * @param {Number} num1 
+ * @param {Number} num2 
+ * @returns 
+ */
 function add(num1, num2) {
   const num1Digits = (num1.toString().split(".")[1] || "").length;
   const num2Digits = (num2.toString().split(".")[1] || "").length;
@@ -10,6 +16,12 @@ function add(num1, num2) {
 
 add(0.1, 0.2);
 
+/**
+ * 精确减法
+ * @param {Number} num1 
+ * @param {Number} num2 
+ * @returns 
+ */
 function multiply(num1, num2) {
   const num1Digits = (num1.toString().split(".")[1] || "").length;
   const num2Digits = (num2.toString().split(".")[1] || "").length;

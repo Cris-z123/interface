5d28090ac5906dd87546530d896b3eeaaa4ba070
rookie996-object <472099249@qq.com>
2024-03-17 23:08:36 +0800

    feat: binary tree traversal

--- js-lib/practice.js
+++ js-lib/practice.js
@@ -51,3 +51,49 @@ function moveZeros(arr) {
     }
     return arr
 }
+
+/**
+ * chunk([1, 2, 3, 4], 2) => [[1, 2], [3, 4]]
+ * @param {*} array 
+ * @param {*} size 
+ * @returns 
+ */
+function chunk(array, size) {
+    const result = []
+    let index = 0
+    while (index < Array.length) {
+        result.push(array.slice(index, index + size))
+        index += size
+    }
+    return result
+}
+
+/**
+ * 二叉树的前序遍历
+ * @param {*} val 
+ * @param {*} left 
+ * @param {*} right 
+ */
+function TreeNode(val, left, right) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : val)
+    this.right = (right === undefined ? null : right)
+}
+
+function preorderTraversal(root) {
+    const res = []
+    if (root === null) {
+        return res
+    }
+    const order = (node) => {
+        res.push(node.val)
+        if (node.left !== null) {
+            order(node.left)
+        }
+        if (node.right !== null) {
+            order(node.right)
+        }
+    }
+    order(root)
+    return res
+}
